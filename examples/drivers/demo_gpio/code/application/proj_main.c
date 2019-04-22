767e36f897ee1bf4609a127dcb2640db6a964822
PAN <pan@freqchip.com>
2019-04-22 23:16:04 +0800

    Mode gpio demo codes added. Set PC2 as OUTPUT and PC3 as INPUT. Please refer to driver_iomux.h for gpio detail.
    Signed-off-by: PAN <pan@freqchip.com>

--- examples/drivers/demo_gpio/code/application/proj_main.c
+++ examples/drivers/demo_gpio/code/application/proj_main.c
@@ -15,6 +15,9 @@
 
 #include "gap_api.h"
 
+#include "driver_iomux.h"
+#include "driver_gpio.h"
+
 void user_proj_main_before_ble_ini(void)
 {
     printf("user proj_main_before_ble_ini\r\n");
@@ -25,9 +28,20 @@ void user_proj_main_before_ble_ini(void)
 
 void user_proj_main(void)
 {
+	printf("Configure PC2 as OUTPUT high\r\n");
 	gpio_set_port_mux(GPIO_PORT_C, GPIO_BIT_2, PORTC2_FUNC_C2);
 	gpio_set_dir(GPIO_PORT_C, GPIO_BIT_2, GPIO_DIR_OUT);
-	gpio_portc_write(gpio_portc_read() & (~(1<<GPIO_BIT_2)));	
+	gpio_portc_write(gpio_portc_read() | ((1<<GPIO_BIT_2)));
+
+	printf("Configure PC3 as INPUT\r\n");
+	gpio_set_port_mux(GPIO_PORT_C, GPIO_BIT_3, PORTC3_FUNC_C3);
+	gpio_set_dir(GPIO_PORT_C, GPIO_BIT_3, GPIO_DIR_IN);
+	
+	if(gpio_portc_read() & BIT(3))
+			printf("PC3 is high\r\n");
+	
+	if(!(gpio_portc_read() & BIT(3)))
+			printf("PC3 is low\r\n");
 }
 
 
